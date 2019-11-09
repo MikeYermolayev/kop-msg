57fa4272794372e15de492ccefcb203759956417
Mikhail Yermolayev <ermolaev@targetprocess.com>
2019-11-09 20:19:44 +0300

    add some styling

--- router.js
+++ router.js
@@ -2,4 +2,14 @@ var app = document.getElementById('app');
 
 import('./login.html').then(function(login) {
     app.innerHTML = login;
+    var scripts = app.querySelectorAll('script');
+
+    for (var j = 0; j < scripts.length; j++) {
+        var script = document.createElement('script');
+        script.type = scripts[j].type || 'text/javascript';
+        if (scripts[j].hasAttribute('src')) script.src = scripts[j].src;
+        script.innerHTML = scripts[j].innerHTML;
+        document.head.appendChild(script);
+        document.head.removeChild(script);
+    }
 });
